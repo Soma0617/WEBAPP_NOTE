fabf518ab89e40b05ed0e3f4f54d3431dcb27adf
Soma0017 <soma0928153146@gmail.com>
2025-08-12 16:13:42 +0800

    筆記更新

--- CodeFirstTeach/Models/SeedData.cs
+++ CodeFirstTeach/Models/SeedData.cs
@@ -1,4 +1,7 @@
-﻿namespace CodeFirstTeach.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace CodeFirstTeach.Models
 {
     public class SeedData
     {
@@ -9,6 +12,33 @@
         (3) 撰寫上傳圖片的程式
         (4) 加上using()及判斷資料庫是否有資料的程式
         */
+        // 撰寫靜態方法Initialize( IServiceProvider serviceProvider )
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            // 撰寫Book及ReBook資料表內的初始資料程式
+            GuestBookContext context = new GuestBookContext(serviceProvider.GetRequiredService<DbContextOptions<GuestBookContext>>());
 
+            context.Book.AddRange(
+                new Book 
+                { 
+                    BookID = Guid.NewGuid().ToString(), // 我們設定的BookID是GUID，而不是自己取的號碼，而且要用ToString()把GUID轉成字串
+                    Title = "C# 程式設計",
+                    Discription = "這是一本關於C#程式設計的書籍",
+                    Author = "作者一號",
+                    Photo = ".jpg",
+                    CreatedDate = DateTime.Now,
+                },
+                {
+                    BookID = "書籍二號",
+                    Title = "烹飪課",
+                    Discription = "這是一本關於烹飪的書籍",
+                    Author = "作者五號",
+                    Photo = ".jpg",
+                    CreatedDate = DateTime.Now,
+                }
+            )
+
+            context.ReBook;
+        }
     }
 }
